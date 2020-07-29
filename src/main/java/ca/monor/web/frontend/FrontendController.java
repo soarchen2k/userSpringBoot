0f49f430803f536d90508475f462ca37da4ebe17
soarchen <chenchong.blog@gmail.com>
2020-07-28 22:25:34 -0400

    add index.html

---
+++ src/main/java/ca/monor/web/frontend/FrontendController.java
@@ -0,0 +1,19 @@
+package ca.monor.web.frontend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/frontend")
+public class FrontendController {
+
+    /**
+     * 首页路由
+     * @return
+     */
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    private String index() {
+        return "frontend/index";
+    }
+}
---
+++ src/main/java/ca/monor/web/frontend/MainPageController.java
@@ -0,0 +1,10 @@
+package ca.monor.web.frontend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/frontend")
+public class MainPageController {
+
+}
